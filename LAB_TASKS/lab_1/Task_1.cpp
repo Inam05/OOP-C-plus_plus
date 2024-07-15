2400ca1d520ffc12c6f40b60fded654781ad2d5b
Inam05 <inamulhassan.tech@gmail.com>
2024-07-15 13:27:53 +0500

    Task_1 => Area of the rectangle with function is added

---
+++ LAB_TASKS/lab_1/Task_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+double perimeter(double length, double width)
+{
+    return 2 * (length + width);
+}
+
+double area(double length, double width)
+{
+    return length * width;
+}
+
+int main()
+{
+    double length, width;
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+    if (length <= 0 || width <= 0)
+    {
+        cout << "Invalid input. Length and width must be positive numbers." << endl;
+        return 1; 
+    }
+    cout << "Perimeter of the rectangle: " << perimeter(length, width) << endl;
+    cout << "Area of the rectangle: " << area(length, width) << endl;
+    return 0; 
+}
