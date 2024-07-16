ce8f3183d2b723a96ae8c8b61405e3219e66647f
Inam05 <inamulhassan.tech@gmail.com>
2024-07-16 23:15:25 +0500

    Question add

--- LAB_TASKS/lab_5/Task_1.cpp
+++ LAB_TASKS/lab_5/Task_1.cpp
@@ -1,3 +1,15 @@
+/*
+Write a C++ program to write following functions described below that use pointers
+and dynamic memory allocation.
+The Functions:
+You will write these functions described below. Then you will call them from the
+main function, to demonstrate their working:
+1. maximum: takes an int array and the array's size as arguments. It should
+return the maximum value of the array elements.
+Do not use square brackets ANYWHERE in the function (use pointers
+instead). Extra challenge: Do not use the loop variable in the body of the
+loop.
+*/
 #include <iostream>
 using namespace std;
 
