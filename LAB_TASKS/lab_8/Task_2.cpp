4b10644def20079bdc44e3317044c31158a3ba40
Inam05 <inamulhassan.tech@gmail.com>
2024-07-16 23:20:16 +0500

    Question add

--- LAB_TASKS/lab_8/Task_2.cpp
+++ LAB_TASKS/lab_8/Task_2.cpp
@@ -1,3 +1,14 @@
+/*
+: Multiple Inheritance
+According to the following algorithm, write a C++ program:
+Step 1: Take two classes teacher and student
+Step 2: Within the teacher class Enter name and teacher Id with the help of getdata()
+function and display this data using putdata() function.
+Step 3: Within the student class Enter and display marks m1,m2,m3 with the help of
+getdata(),putdata() functions.
+Step 4: Within the student class extends teacher class and using data and their functions to
+the student class. 
+*/
 #include <iostream>
 #include <string>
 using namespace std;
