03d4e3feeb52360141ed482f6567ca8f5b06ca4b
Inam05 <inamulhassan.tech@gmail.com>
2024-07-17 22:30:01 +0500

    Question add

---
+++ LAB_TASKS/lab_9/Task_3.cpp
@@ -0,0 +1,7 @@
+/*
+Create a base class called Geometry with virtual functions
+calculateArea() and calculatePerimeter(). Derive subclasses Circle, Rectangle,
+and Triangle from Geometry. Implement the calculateArea() and
+calculatePerimeter() functions differently in each subclass to calculate the area
+and perimeter of the respective shapes.
+*/
