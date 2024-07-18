375e466ceb28de89604b1d7849e8cf1ff676bc38
Inam05 <inamulhassan.tech@gmail.com>
2024-07-18 22:53:06 +0500

    File added

---
+++ Self_practice/Class_Design_and_Inheritance.cpp
@@ -0,0 +1,5 @@
+/*
+Design a class hierarchy for a company with Employee, Manager, Engineer, and Intern classes. 
+Implement common methods and unique functionalities for each class. Ensure proper use of access 
+specifiers, constructors, and destructors.
+*/
