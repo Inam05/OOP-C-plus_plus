ad5b0e5b9707036976cdf1b896e1f4f7b35039fc
Inam05 <inamulhassan.tech@gmail.com>
2024-07-18 23:00:46 +0500

    Solution added of 'Polymorphism and Virtual Functions'

--- Self_practice/Polymorphism_and_Virtual_Functions.cpp
+++ Self_practice/Polymorphism_and_Virtual_Functions.cpp
@@ -1,5 +1,57 @@
 /*
-Create a base class Shape with derived classes Circle, Rectangle, and Triangle. Implement a 
-virtual function draw() in the base class and override it in the derived classes. 
+Create a base class Shape with derived classes Circle, Rectangle, and Triangle. Implement a
+virtual function draw() in the base class and override it in the derived classes.
 Use polymorphism to draw different shapes.
 */
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual void draw() const = 0;
+    virtual ~Shape() {}
+};
+
+class Circle : public Shape
+{
+public:
+    void draw() const override
+    {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+class Rectangle : public Shape
+{
+public:
+    void draw() const override
+    {
+        cout << "Drawing Rectangle" << endl;
+    }
+};
+
+class Triangle : public Shape
+{
+public:
+    void draw() const override
+    {
+        cout << "Drawing Triangle" << endl;
+    }
+};
+
+int main()
+{
+    Circle c;
+    Rectangle r;
+    Triangle t;
+
+    Shape *shapes[] = {&c, &r, &t};
+
+    for (int i = 0; i < 3; ++i)
+    {
+        shapes[i]->draw();
+    }
+
+    return 0;
+}
