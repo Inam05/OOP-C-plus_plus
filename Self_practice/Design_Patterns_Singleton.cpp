caf780c1390f46f4f39aac5d2cbc57165953b5bb
Inam05 <inamulhassan.tech@gmail.com>
2024-07-20 23:16:34 +0500

    Solution add

--- Self_practice/Design_Patterns_Singleton.cpp
+++ Self_practice/Design_Patterns_Singleton.cpp
@@ -1,3 +1,42 @@
 /*
 Implement the Singleton design pattern for a Logger class that logs messages to a file. Ensure thread safety and proper resource management.
 */
+
+#include <iostream>
+using namespace std;
+
+class Base1
+{
+public:
+    void greet()
+    {
+        cout << "Hello from Base1" << endl;
+    }
+};
+
+class Base2
+{
+public:
+    void greet()
+    {
+        cout << "Hello from Base2" << endl;
+    }
+};
+
+class Hybrid : public Base1, public Base2
+{
+public:
+    void greet()
+    {
+        Base1::greet(); // Resolving ambiguity
+        Base2::greet();
+    }
+};
+
+int main()
+{
+    Hybrid h;
+    h.greet();
+
+    return 0;
+}
