5c6d59285e87261e11f0d330417ffd2908f08fff
Inam05 <inamulhassan.tech@gmail.com>
2024-07-20 23:10:13 +0500

    File and question added

---
+++ Self_practice/Abstract Classes_and_Interfaces.cpp
@@ -0,0 +1,4 @@
+/*
+Define an abstract class Animal with pure virtual functions makeSound() and move(). Create derived classes Dog, Cat, and Bird that implement 
+these functions. Use the abstract class to demonstrate polymorphism.
+*/
