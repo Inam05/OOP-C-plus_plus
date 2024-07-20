831cd77b37ccd7e2cad42001b54204e9eeca1c1a
Inam05 <inamulhassan.tech@gmail.com>
2024-07-20 23:17:22 +0500

    Solution added

--- Self_practice/Abstract Classes_and_Interfaces.cpp
+++ Self_practice/Abstract Classes_and_Interfaces.cpp
@@ -2,3 +2,42 @@
 Define an abstract class Animal with pure virtual functions makeSound() and move(). Create derived classes Dog, Cat, and Bird that implement 
 these functions. Use the abstract class to demonstrate polymorphism.
 */
+
+#include <iostream>
+using namespace std;
+
+class Base1
+{
+public:
+    void greet()
+    {
+        cout << "Hello from Base1" << endl;
+    }
+};
+
+class Base2
+{
+public:
+    void greet()
+    {
+        cout << "Hello from Base2" << endl;
+    }
+};
+
+class Hybrid : public Base1, public Base2
+{
+public:
+    void greet()
+    {
+        Base1::greet(); // Resolving ambiguity
+        Base2::greet();
+    }
+};
+
+int main()
+{
+    Hybrid h;
+    h.greet();
+
+    return 0;
+}
