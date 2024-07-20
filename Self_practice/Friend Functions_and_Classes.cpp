7902532107e2b567f3b007ffa9d00f1e8b171e23
Inam05 <inamulhassan.tech@gmail.com>
2024-07-20 23:13:21 +0500

    File and question added

---
+++ Self_practice/Friend Functions_and_Classes.cpp
@@ -0,0 +1,4 @@
+/*
+Create a Rectangle class and a Square class. Implement a friend function that checks if a square can fit inside a rectangle. Also, 
+make one class a friend of the other and demonstrate the usage.
+*/
