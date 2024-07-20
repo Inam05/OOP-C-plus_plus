a9b7843427c65ab37962947e11470f49d5b09a2d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-20 23:09:16 +0500

    File and question added

---
+++ Self_practice/Multiple_Inheritance.cpp
@@ -0,0 +1,4 @@
+/*
+Create a scenario with multiple inheritance where a class Hybrid inherits from two base classes Base1 and Base2. 
+Implement a method in Hybrid that resolves ambiguity arising from multiple inheritance.
+*/
