20f778a88271ca617e06a8fe162df746810aa7a3
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:38:00 +0500

    Tic-Tac-Toe Game Project Folder and file added

---
+++ Projects/7. Tic-Tac-Toe Game/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A console-based game of Tic-Tac-Toe with a basic AI opponent.
+Questions:
+How will you represent the game board and moves?
+How will you implement game logic and AI?
+What features will you include for user interaction?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code 
