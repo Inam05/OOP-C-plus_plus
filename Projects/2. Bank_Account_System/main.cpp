88474b3b06e1e6c6d37f502ba31a5f4f506054b0
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:24:02 +0500

    Bank Account System Project Folder and file added with Description and questions

---
+++ Projects/2. Bank_Account_System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system to manage bank accounts, including deposits, withdrawals, and balance inquiries.
+Questions:
+How will you manage multiple accounts?
+How will you handle transactions and balance updates?
+What security measures will you implement?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
