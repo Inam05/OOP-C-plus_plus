011485bcbd15e16631238a05083ca32626099ca6
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 21:52:50 +0500

    File and question added

---
+++ Self_practice/STL_Integration.cpp
@@ -0,0 +1,4 @@
+/*
+Design a custom container class that integrates with the C++ Standard Template Library (STL). Implement iterators and necessary member functions 
+to make it compatible with STL algorithms.
+*/
