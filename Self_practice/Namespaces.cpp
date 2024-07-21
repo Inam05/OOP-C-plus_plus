8cc0ea647b82f905246421e35c83900ddfcafaad
Inam05 <inamulhassan.tech@gmail.com>
2024-07-22 00:38:59 +0500

    Solution added

--- Self_practice/Namespaces.cpp
+++ Self_practice/Namespaces.cpp
@@ -1,4 +1,32 @@
 /*
-Create a namespace Math that contains various mathematical functions like add, subtract, multiply, 
+Create a namespace Math that contains various mathematical functions like add, subtract, multiply,
 and divide. Demonstrate the usage of these functions within and outside the namespace.
 */
+
+#include <iostream>
+using namespace std;
+
+namespace Geometry
+{
+    class Point
+    {
+    private:
+        int x, y;
+
+    public:
+        Point(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
+
+        void display()
+        {
+            cout << "Point (" << x << ", " << y << ")" << endl;
+        }
+    };
+}
+
+int main()
+{
+    Geometry::Point p(3, 4);
+    p.display();
+
+    return 0;
+}
