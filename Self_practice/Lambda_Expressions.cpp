9dbdca59d8e8da45cef534271b8173ae070b8786
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:01:24 +0500

    File and question added

---
+++ Self_practice/Lambda_Expressions.cpp
@@ -0,0 +1,4 @@
+/*
+Use lambda expressions to implement a custom sorting function for a std::vector of Person objects based 
+on their age and name. Demonstrate the usage of the lambda function in std::sort.
+*/
