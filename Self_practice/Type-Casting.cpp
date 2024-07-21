f03174a124169cec81ca534871e8c629047448d9
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:02:09 +0500

    File and question added

---
+++ Self_practice/Type-Casting.cpp
@@ -0,0 +1,4 @@
+/*
+Demonstrate the use of different type casting operators (static_cast, dynamic_cast, const_cast, and reinterpret_cast) in various scenarios. Provide 
+code examples for each type cast and explain their use cases.
+*/
