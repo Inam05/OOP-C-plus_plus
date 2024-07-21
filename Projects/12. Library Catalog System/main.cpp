3c1a082c0c254820c75f8c5da1d844ba0182351f
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:58:11 +0500

    Library Catalog System Project Folder and file have been added

---
+++ Projects/12. Library Catalog System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system for managing and categorizing library books.
+Questions:
+How will you structure book categories and metadata?
+How will you handle book searches and recommendations?
+What features will you include for managing book catalogs?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code 
