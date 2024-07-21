8ab63234d46d7ed01413e23e5504117ab4f4dd96
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:26:23 +0500

    Student Management System Project Folder and file added

---
+++ Projects/3. Student_Management_System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system for managing student records, including enrollment, grades, and courses.
+Questions:
+How will you handle student data and course information?
+How will you calculate and store grades?
+How will you generate student reports and transcripts?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
