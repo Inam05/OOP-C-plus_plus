c7971102e960c9c702ddf34b736354523c336eb4
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:31:33 +0500

    Hotel Reservation System Project Folder and file added

---
+++ Projects/6. Hotel Reservation System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system to manage hotel room reservations and guest information.
+Questions:
+How will you manage room availability and bookings?
+How will you handle guest check-ins and check-outs?
+What features will you include for managing reservations?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
