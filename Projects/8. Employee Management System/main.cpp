16c1bbea2d9dfd7803e9c6e1628cb541846854fd
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:48:48 +0500

    Employee Management System Project Folder and file have been added

---
+++ Projects/8. Employee Management System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system for managing employee records, payroll, and departments.
+Questions:
+How will you handle employee information and payroll details?
+How will you manage department assignments and reports?
+What features will you include for managing employee data?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
