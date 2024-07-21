6eb85621f836fb7361966f4725a392fe20650fca
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 21:58:41 +0500

    File and question added

---
+++ Self_practice/File_I/O_and_Serialization.cpp
@@ -0,0 +1,4 @@
+/*
+Design a Student class with data members name, age, and grades. 
+Implement methods to read and write Student objects to and from a file. Ensure proper serialization and deserialization
+*/
