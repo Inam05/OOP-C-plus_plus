e2202b9f407b14c6d017b231e5750870dcfe193e
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:27:51 +0500

    Online Shopping System Project File and folder added

---
+++ Projects/4. Online Shopping System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system for managing products, customer accounts, and orders.
+Questions:
+How will you handle product listings and inventory?
+How will you process and manage orders?
+What features will you include for managing customer accounts?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+//code
