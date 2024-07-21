b496c9b5f889a66c3961c7681e12a633a94f38d8
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:50:16 +0500

    Hospital Management System Project Folder and file have been added

---
+++ Projects/9. Hospital Management System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system to manage patient records, appointments, and medical staff.
+Questions:
+How will you handle patient information and medical records?
+How will you manage appointments and staff schedules?
+What features will you include for managing hospital operations?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
