5de5f638f911e2394a55e62d354cfb379c9e82d3
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:20:25 +0500

    Library Management System folder and file with description and questions added

---
+++ Projects/1. Library_Management_System/main.cpp
@@ -0,0 +1,10 @@
+/*
+Project Description
+A system to manage books, members, and transactions.
+Questions
+How will you structure the data for books and members?
+How will you handle book borrowing and returning?
+What features will you include for searching and managing books?
+*/
+
+// code :
