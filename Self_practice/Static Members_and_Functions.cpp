376e923bdb35f4bfda49054d454f258aecbbe1d2
Inam05 <inamulhassan.tech@gmail.com>
2024-07-22 00:37:39 +0500

    Solution has been added

--- Self_practice/Static Members_and_Functions.cpp
+++ Self_practice/Static Members_and_Functions.cpp
@@ -1,4 +1,37 @@
 /*
-Implement a Counter class with a static member variable to keep track of the number of Counter objects created. 
+Implement a Counter class with a static member variable to keep track of the number of Counter objects created.
 Provide static member functions to access and manipulate this count.
 */
+
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+private:
+    static int count;
+
+public:
+    Counter()
+    {
+        ++count;
+    }
+
+    static int getCount()
+    {
+        return count;
+    }
+};
+
+int Counter::count = 0;
+
+int main()
+{
+    Counter c1;
+    Counter c2;
+    Counter c3;
+
+    cout << "Number of Counter objects: " << Counter::getCount() << endl;
+
+    return 0;
+}
