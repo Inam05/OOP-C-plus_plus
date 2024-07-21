545135cc9aaf1520196857ca8d896439adb11b36
Inam05 <inamulhassan.tech@gmail.com>
2024-07-22 00:43:18 +0500

    Solution has been added

--- Self_practice/O_and_Serialization.cpp
+++ Self_practice/O_and_Serialization.cpp
@@ -1,4 +1,40 @@
 /*
-Design a Student class with data members name, age, and grades. 
+Design a Student class with data members name, age, and grades.
 Implement methods to read and write Student objects to and from a file. Ensure proper serialization and deserialization
 */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream outFile("example.txt");
+    if (outFile.is_open())
+    {
+        outFile << "Hello, world!" << endl;
+        outFile << "This is a test file." << endl;
+        outFile.close();
+    }
+    else
+    {
+        cout << "Unable to open file for writing" << endl;
+    }
+
+    ifstream inFile("example.txt");
+    if (inFile.is_open())
+    {
+        string line;
+        while (getline(inFile, line))
+        {
+            cout << line << endl;
+        }
+        inFile.close();
+    }
+    else
+    {
+        cout << "Unable to open file for reading" << endl;
+    }
+
+    return 0;
+}
