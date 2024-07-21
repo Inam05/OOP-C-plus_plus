97ad556ec89901e81c2713c8d55524a3cd0fd189
Inam05 <inamulhassan.tech@gmail.com>
2024-07-22 00:39:56 +0500

    Solution added

--- Self_practice/Lambda_Expressions.cpp
+++ Self_practice/Lambda_Expressions.cpp
@@ -1,4 +1,25 @@
 /*
-Use lambda expressions to implement a custom sorting function for a std::vector of Person objects based 
+Use lambda expressions to implement a custom sorting function for a std::vector of Person objects based
 on their age and name. Demonstrate the usage of the lambda function in std::sort.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto add = [](int a, int b) -> int
+    {
+        return a + b;
+    };
+
+    auto multiply = [](int a, int b) -> int
+    {
+        return a * b;
+    };
+
+    cout << "Addition: " << add(5, 3) << endl;
+    cout << "Multiplication: " << multiply(5, 3) << endl;
+
+    return 0;
+}
