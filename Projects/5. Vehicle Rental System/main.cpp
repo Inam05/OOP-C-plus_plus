3f6b8b93e0d49c8138d7df8c51bfb6e156686cb3
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:30:07 +0500

    Vehicle Rental System Project Folder and file added

---
+++ Projects/5. Vehicle Rental System/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A system to manage vehicle rentals, including availability and customer transactions.
+Questions:
+How will you track vehicle availability and rental status?
+How will you handle rental agreements and transactions?
+What features will you include for managing customer information?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
