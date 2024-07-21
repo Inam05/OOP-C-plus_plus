3b931c0dbe66d6e47b2288fe58403dd94cfe23a8
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 22:52:22 +0500

    Chess Game Project Folder and file added

---
+++ Projects/10. Chess Game/main.cpp
@@ -0,0 +1,14 @@
+/*
+Description: A console-based Chess game with basic AI for playing against the computer.
+Questions:
+How will you represent the game board and pieces?
+How will you implement chess rules and moves?
+What features will you include for user interaction and AI?
+*/
+
+/* 
+    Created by Inam Ul Hassan 
+    Github : https://github.com/Inam05
+*/
+
+// code
