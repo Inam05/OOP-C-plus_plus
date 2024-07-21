b6ad71a666603d4f5b173b6debf88fd94715de97
Inam05 <inamulhassan.tech@gmail.com>
2024-07-21 21:55:44 +0500

    File and question added

---
+++ Self_practice/Namespaces.cpp
@@ -0,0 +1,4 @@
+/*
+Create a namespace Math that contains various mathematical functions like add, subtract, multiply, 
+and divide. Demonstrate the usage of these functions within and outside the namespace.
+*/
