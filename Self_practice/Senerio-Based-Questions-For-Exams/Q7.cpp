64d2737bf9db88ed117aa9a662efb0a28c586def
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:47:23 +0500

    Question 7 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q7.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Animal with attributes name and species. Derive classes Bird and Mammal from Animal. Implement a virtual method 
+makeSound() in the base class and override it in the derived classes to simulate different sounds made by birds and mammals.
+*/
