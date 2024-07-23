778332d7541e7491b73e3cc4b869b4756962b2ba
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:38:10 +0500

    Question 2 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q2.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class Book with attributes title, author, price, and stockQuantity. Implement a method to purchase a book which updates the 
+stock quantity. How would you extend this class to handle different genres of books?
+*/
