7da6bbcfe209db5fdad75e5bb1a1b841e7b686d4
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:35:12 +0500

    Folder created related to senerio based questions for exam prepration

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q1.cpp
@@ -0,0 +1,4 @@
+/*
+Design a BankAccount class with attributes like accountNumber, accountHolderName, and balance. 
+mplement methods for deposit, withdraw, and checkBalance. How would you handle overdraft scenarios in the withdraw method?
+*/
