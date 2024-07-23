a7c4cfc8e014d89adaf83ca5f7cb6313a976cc2f
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:41:56 +0500

    Question 4 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q4.cpp
@@ -0,0 +1,4 @@
+/*
+Design a base class Shape with a pure virtual method draw(). Derive classes Circle, Rectangle, and Triangle from Shape. Implement 
+the draw() method in each derived class to draw the specific shape.
+*/
