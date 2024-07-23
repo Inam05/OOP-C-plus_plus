8ffdec805240ed5482d54657cb3dbc530bfc1d96
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:48:33 +0500

    Question 8 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q8.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class LibraryItem with attributes title and itemID. Derive classes Book and Magazine from LibraryItem. Implement a virtual 
+method borrowItem() in the base class and override it in the derived classes to handle borrowing books and magazines differently.
+*/
