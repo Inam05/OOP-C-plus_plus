48c6c74e17849dd126815039a16841c192c14740
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:45:42 +0500

    Question 6 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q6.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class Media with attributes title and duration. Derive classes Audio and Video from Media. Implement a virtual method play() 
+in the base class and override it in the derived classes to handle playing audio and video files differently.
+*/
