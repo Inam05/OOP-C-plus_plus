fe7a2d6b52af15cc6d8c557e0cccc3ac0481b890
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:49:25 +0500

    Question 9 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q9.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Employee with attributes employeeID and name. Derive classes Manager and Engineer from Employee. Implement a 
+virtual method calculateSalary() in the base class and override it in the derived classes to calculate salaries based on the employee type.
+*/
