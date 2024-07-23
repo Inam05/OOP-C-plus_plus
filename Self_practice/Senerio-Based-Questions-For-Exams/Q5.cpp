a6da175c3ef61a694fc3d4fd1ea1cad996347e1b
Inam05 <inamulhassan.tech@gmail.com>
2024-07-23 21:43:38 +0500

    Question 5 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q5.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class PaymentMethod with a virtual method processPayment(). Derive classes CreditCard and PayPal from PaymentMethod. 
+Implement processPayment() in both derived classes to process payments differently based on the payment method.
+*/
