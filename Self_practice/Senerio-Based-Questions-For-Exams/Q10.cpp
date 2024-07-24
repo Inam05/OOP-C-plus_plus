bb2d9a7f8c240bb54d12aaacaf925df0ae202e07
Inam05 <inamulhassan.tech@gmail.com>
2024-07-24 22:25:07 +0500

    Question 10 added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q10.cpp
@@ -0,0 +1,8 @@
+/*
+Design a class GameObject with a virtual method update(). Derive classes Player and Enemy from GameObject. Implement the update()
+method in each derived class to handle updates specific to player and enemy objects in the game.
+*/
+
+// code
+
+
