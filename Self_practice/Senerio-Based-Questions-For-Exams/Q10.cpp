6048519a92a5ee004cde5c850406f7a98c4baf66
Inam05 <inamulhassan.tech@gmail.com>
2024-07-24 22:25:24 +0500

    Solution added

--- Self_practice/Senerio-Based-Questions-For-Exams/Q10.cpp
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q10.cpp
@@ -6,3 +6,40 @@ method in each derived class to handle updates specific to player and enemy obje
 // code
 
 
+#include <iostream>
+using namespace std;
+
+class GameObject
+{
+public:
+    virtual void update() = 0; // Pure virtual function
+};
+
+class Player : public GameObject
+{
+public:
+    void update() override
+    {
+        cout << "Updating Player's position" << endl;
+    }
+};
+
+class Enemy : public GameObject
+{
+public:
+    void update() override
+    {
+        cout << "Updating Enemy's position" << endl;
+    }
+};
+
+int main()
+{
+    GameObject *g1 = new Player();
+    GameObject *g2 = new Enemy();
+    g1->update();
+    g2->update();
+    delete g1;
+    delete g2;
+    return 0;
+}
