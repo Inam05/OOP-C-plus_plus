2b47eb02c365193a79a96fa03bbf15299300e3a5
Inam05 <inamulhassan.tech@gmail.com>
2024-07-24 22:21:47 +0500

    Solution added

--- Self_practice/Senerio-Based-Questions-For-Exams/Q5.cpp
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q5.cpp
@@ -1,4 +1,43 @@
 /*
-Create a class PaymentMethod with a virtual method processPayment(). Derive classes CreditCard and PayPal from PaymentMethod. 
+Create a class PaymentMethod with a virtual method processPayment(). Derive classes CreditCard and PayPal from PaymentMethod.
 Implement processPayment() in both derived classes to process payments differently based on the payment method.
 */
+
+// code
+#include <iostream>
+using namespace std;
+
+class PaymentMethod
+{
+public:
+    virtual void processPayment() = 0; // Pure virtual function
+};
+
+class CreditCard : public PaymentMethod
+{
+public:
+    void processPayment() override
+    {
+        cout << "Processing Credit Card Payment" << endl;
+    }
+};
+
+class PayPal : public PaymentMethod
+{
+public:
+    void processPayment() override
+    {
+        cout << "Processing PayPal Payment" << endl;
+    }
+};
+
+int main()
+{
+    PaymentMethod *pm1 = new CreditCard();
+    PaymentMethod *pm2 = new PayPal();
+    pm1->processPayment();
+    pm2->processPayment();
+    delete pm1;
+    delete pm2;
+    return 0;
+}
--- Self_practice/Senerio-Based-Questions-For-Exams/Q6.cpp
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q6.cpp
@@ -5,40 +5,4 @@ in the base class and override it in the derived classes to handle playing audio
 
 // code
 
-#include <iostream>
-using namespace std;
 
-class PaymentMethod
-{
-public:
-    virtual void processPayment() = 0; // Pure virtual function
-};
-
-class CreditCard : public PaymentMethod
-{
-public:
-    void processPayment() override
-    {
-        cout << "Processing Credit Card Payment" << endl;
-    }
-};
-
-class PayPal : public PaymentMethod
-{
-public:
-    void processPayment() override
-    {
-        cout << "Processing PayPal Payment" << endl;
-    }
-};
-
-int main()
-{
-    PaymentMethod *pm1 = new CreditCard();
-    PaymentMethod *pm2 = new PayPal();
-    pm1->processPayment();
-    pm2->processPayment();
-    delete pm1;
-    delete pm2;
-    return 0;
-}
