b53a876ac304c451d7f22210f9b94fbb05fb7c72
Inam05 <inamulhassan.tech@gmail.com>
2024-07-24 22:22:11 +0500

    Solution added

--- Self_practice/Senerio-Based-Questions-For-Exams/Q6.cpp
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q6.cpp
@@ -5,4 +5,46 @@ in the base class and override it in the derived classes to handle playing audio
 
 // code
 
+#include <iostream>
+using namespace std;
 
+class Media
+{
+protected:
+    string title;
+    int duration; // duration in seconds
+public:
+    Media(string t, int d) : title(t), duration(d) {}
+    virtual void play() = 0; // Pure virtual function
+};
+
+class Audio : public Media
+{
+public:
+    Audio(string t, int d) : Media(t, d) {}
+    void play() override
+    {
+        cout << "Playing Audio: " << title << " for " << duration << " seconds" << endl;
+    }
+};
+
+class Video : public Media
+{
+public:
+    Video(string t, int d) : Media(t, d) {}
+    void play() override
+    {
+        cout << "Playing Video: " << title << " for " << duration << " seconds" << endl;
+    }
+};
+
+int main()
+{
+    Media *m1 = new Audio("Song.mp3", 180);
+    Media *m2 = new Video("Movie.mp4", 7200);
+    m1->play();
+    m2->play();
+    delete m1;
+    delete m2;
+    return 0;
+}
