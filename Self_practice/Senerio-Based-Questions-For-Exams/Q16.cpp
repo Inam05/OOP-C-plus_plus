edc6651528cd7489536d18068a49396bfda17302
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:56:55 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q16.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class GameCharacter with attributes name and health. Derive classes Warrior and Mage from GameCharacter. Implement 
+polymorphic behavior for a method attack() to simulate different attack styles for warriors and mages.
+*/
