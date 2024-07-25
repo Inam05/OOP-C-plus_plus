5aded4e9a434fd3a3f0f753bd7bafcee6d543325
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:53:08 +0500

    Task 12 Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q12.cpp
@@ -0,0 +1,5 @@
+/*
+Design a class InventoryItem with attributes itemID, name, and quantity. Derive classes PerishableItem and NonPerishableItem from 
+InventoryItem. Implement a virtual method checkExpiration() in the base class and override it in the derived class PerishableItem to 
+handle expiration checks.
+*/
