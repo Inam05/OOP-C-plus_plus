f028652c9650f29add2b234d01574146774a62ae
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:53:57 +0500

    Task 13 Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q13.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Person with attributes name and age. Derive classes Student and Faculty from Person. Implement polymorphic 
+behavior for a method displayDetails() to display details specific to students and faculty members.
+*/
