1cdc3f817f5b9341bc0922b2e94e41f5217fd179
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:58:01 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q17.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Booking with attributes bookingID and customerName. Derive classes FlightBooking and HotelBooking from Booking. 
+Implement a virtual method confirmBooking() to confirm bookings for flights and hotels.
+*/
