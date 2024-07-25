12ac609042c7391962bbec4d48394c1c79d84413
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:52:13 +0500

    Task 11 Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q11.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Employee with attributes employeeID and name. Derive classes Chef, Waiter, and Manager from Employee. Implement 
+polymorphic behavior for a method getRoleDescription() to describe the role of each type of employee.
+*/
