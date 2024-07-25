95ce72ba720fc1667a5bfed2bf5982a803f94199
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:58:44 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q18.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class LibraryItem with attributes title and itemID. Derive classes EBook and PhysicalBook from LibraryItem. Implement a 
+virtual method read() to handle reading an eBook and a physical book differently.
+*/
