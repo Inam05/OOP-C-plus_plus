af0d130226cd6be321d83162b5b9f75ad0173b5d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:59:20 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q19.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class Product with attributes productID, name, and price. Derive classes FoodProduct and ElectronicsProduct from Product. 
+Implement polymorphic behavior for a method applyDiscount() to apply different discount rates for food and electronics products.
+*/
