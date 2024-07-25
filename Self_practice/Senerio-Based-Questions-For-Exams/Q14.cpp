2d20c0b8362ff536cf3437d1d46bcfcdee8899ee
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:54:47 +0500

    Task 14 Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q14.cpp
@@ -0,0 +1,4 @@
+/*
+Design a base class Vehicle with attributes licensePlate and model. Derive classes Bus and Taxi from Vehicle. Implement a virtual 
+method calculateFare() in the base class and override it in the derived classes to calculate fares based on the vehicle type.
+*/
