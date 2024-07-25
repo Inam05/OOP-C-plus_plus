3ef7bebe09720a4a1fccae28ab28647af2f63232
Inam05 <inamulhassan.tech@gmail.com>
2024-07-25 21:59:56 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q20.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class SmartDevice with attributes deviceID and status. Derive classes SmartLight and SmartThermostat from SmartDevice. 
+Implement a virtual method operate() to handle the operation of lights and thermostats.
+*/
