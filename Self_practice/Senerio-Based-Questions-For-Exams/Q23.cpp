5cf0b47745b79d004b950ecfbde5eb0d5a48f5e7
Inam05 <inamulhassan.tech@gmail.com>
2024-07-26 22:40:36 +0500

    Solution added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q23.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class Player with attributes playerID, name, and team. Derive classes FootballPlayer and BasketballPlayer from Player. 
+Implement all four types of constructors for Player. Add a method displayInfo() to demonstrate polymorphism.
+*/
