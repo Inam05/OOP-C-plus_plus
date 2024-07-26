b8c3b185d6ce4b809014795d155f2a22cc03e609
Inam05 <inamulhassan.tech@gmail.com>
2024-07-26 22:39:19 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q21.cpp
@@ -0,0 +1,4 @@
+/*
+Create a base class Animal with attributes name and age, and derive classes Dog and Cat. Implement default, parameterized, copy, 
+and move constructors for Animal. Add a method makeSound() to demonstrate polymorphism.
+*/
