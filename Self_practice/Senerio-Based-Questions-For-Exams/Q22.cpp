2eaa5eaef8c877a555d42cbfdce0eb9a0622fe5e
Inam05 <inamulhassan.tech@gmail.com>
2024-07-26 22:39:56 +0500

    Solution added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q22.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class MusicTrack with attributes title, artist, and duration. Derive classes MP3Track and CDTrack from MusicTrack. Implement 
+all four types of constructors for MusicTrack. Add a method play() to demonstrate polymorphism.
+*/
