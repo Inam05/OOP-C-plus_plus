66d9e0857bc6db6934c176be9f09924436e20eff
Inam05 <inamulhassan.tech@gmail.com>
2024-07-26 22:41:27 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q24.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class RentalVehicle with attributes vehicleID, brand, and ratePerDay. Derive classes Car and Bike from RentalVehicle. 
+Implement all four types of constructors for RentalVehicle. Add a method rent() to demonstrate polymorphism.
+*/
