367b7e36a3517789d601302213b68c11f3a6bb7f
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:24:14 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q26.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class Person with attributes name and age. Derive classes Teacher and Student from Person. Implement all four types of 
+constructors for Person. Add a method displayRole() to demonstrate polymorphism.
+*/
