ebbd8499e34279da18db0500d6e8eda1f435265d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:25:38 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q28.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class Artwork with attributes title, artist, and year. Derive classes Painting and Sculpture from Artwork. Implement all four 
+types of constructors for Artwork. Add a method display() to demonstrate polymorphism.
+*/
