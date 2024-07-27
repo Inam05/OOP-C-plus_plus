a5f661fd4b0df6fbdba958daf3e8c20f0fa4c888
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:26:58 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q30.cpp
@@ -0,0 +1,4 @@
+/*
+Design a class LibraryItem with attributes title, author, and publicationYear. Derive classes Book and Journal from LibraryItem. 
+Implement all four types of constructors for LibraryItem. Add a method borrow() to demonstrate polymorphism.
+*/
