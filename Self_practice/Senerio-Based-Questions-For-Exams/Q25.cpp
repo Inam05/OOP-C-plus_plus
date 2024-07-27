218bc930cb33b7569c479dfe6b1ff5f381ded4aa
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:22:21 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q25.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class Product with attributes productID, name, and price. Derive classes Clothing and Electronics from Product. Implement all 
+four types of constructors for Product. Add a method applyDiscount() to demonstrate polymorphism.
+*/
