ee950cef4dd1641316e00c608b5f328ddd4cc08a
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:24:53 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q27.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class Property with attributes propertyID, address, and price. Derive classes House and Apartment from Property. Implement all 
+four types of constructors for Property. Add a method showDetails() to demonstrate polymorphism.
+*/
