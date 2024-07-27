bc051b0507edfe79e386b753cc185c7e4692717d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-27 21:26:15 +0500

    Question added

---
+++ Self_practice/Senerio-Based-Questions-For-Exams/Q29.cpp
@@ -0,0 +1,4 @@
+/*
+Create a class BankAccount with attributes accountNumber, holderName, and balance. Derive classes SavingsAccount and CheckingAccount 
+from BankAccount. Implement all four types of constructors for BankAccount. Add a method calculateInterest() to demonstrate polymorphism.
+*/
