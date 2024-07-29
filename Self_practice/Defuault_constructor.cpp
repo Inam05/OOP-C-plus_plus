e9784b77653dff11680d63a0ec0c9da9ef145bce
Inam05 <inamulhassan.tech@gmail.com>
2024-07-29 19:40:45 +0500

    Default constructor and parametrized constructor

---
+++ Self_practice/Defuault_constructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    A()
+    {
+        cout << "A:default" << endl;
+    }
+    ~A()
+    {
+        cout << "A:default" << endl;
+    }
+    A(int a)
+    {
+        cout << "A:parameter" << endl;
+    }
+};
+class B : public A
+{
+public:
+    B(int a)
+    {
+        cout << "B" << endl;
+    }
+};
+int main()
+{
+    B test(1);
+}
